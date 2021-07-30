5fe8da5820c56095633886162239a5b474f5b28e
Zack Brown <zack.t.brown@gmail.com>
2021-07-30 10:50:33 -0400

    first commit

---
+++ PrimeService.tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    [TestFixture]
+    public class PrimeService_IsPrimeShould
+    {
+        private PrimeService _primeService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+    }
+}
